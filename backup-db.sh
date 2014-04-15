6b595ba755a42c005332cca9054e2a5a9aead62f
RobertTheNerd <robert.q.wang@gmail.com>
2014-04-15 16:48:49 -0700

    Correct typo.
    From $BackkupFile to $BackupFile on line 14

--- backup-db.sh
+++ backup-db.sh
@@ -11,7 +11,7 @@ NOW=$(date +"%Y-%m-%d_%H-%M-%S")
 BackupPath='/srv/backup/db/'		# set your backup folder
 BackupFile=dbname.$NOW.sql.gz		# set your backup file name
 
-/usr/bin/mysqldump -u dbuser -pdbpassword -h hostname dbname | gzip -c -9 | cat > $BackupPath$BackkupFile
+/usr/bin/mysqldump -u dbuser -pdbpassword -h hostname dbname | gzip -c -9 | cat > $BackupPath$BackupFile
 
 # remove backup file that are over 30 days. Use -cmin for files of certain minutes old.
 find $BackupPath -name '*.sql.gz' -ctime 30 -exec rm {} \;

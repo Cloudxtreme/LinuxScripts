8f3bd1f0970994e8c7544879a041862002fb38fa
RobertTheNerd <robert.q.wang@gmail.com>
2014-04-15 16:46:44 -0700

    Update backup-db.sh

--- backup-db.sh
+++ backup-db.sh
@@ -2,7 +2,8 @@
 
 ###################################
 # Note to restore:
-# Use gzip -d file.sql.gz will decompress the file in place and the original .gz file will be removed. An alternative is to use zcat file.sql.gz > file.sql.
+# Use gzip -d file.sql.gz will decompress the file in place and the original .gz file will be removed. 
+# An alternative is to use zcat file.sql.gz > file.sql.
 ###################################
 
 NOW=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -10,7 +11,7 @@ NOW=$(date +"%Y-%m-%d_%H-%M-%S")
 BackupPath='/srv/backup/db/'		# set your backup folder
 BackupFile=dbname.$NOW.sql.gz		# set your backup file name
 
-/usr/bin/mysqldump -u dbuser -pdbpassword dbname | gzip -c -9 | cat > $BackupPath$BackkupFile
+/usr/bin/mysqldump -u dbuser -pdbpassword -h hostname dbname | gzip -c -9 | cat > $BackupPath$BackkupFile
 
 # remove backup file that are over 30 days. Use -cmin for files of certain minutes old.
-find $BackupPath -name '*.sql.gz' -ctime 60 -exec rm {} \;
+find $BackupPath -name '*.sql.gz' -ctime 30 -exec rm {} \;

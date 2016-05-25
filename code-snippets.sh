e459f87380a78725732baca1d80f9a8af517319c
RobertTheNerd <robert.q.wang@gmail.com>
2016-05-24 19:18:52 -0700

    # unzip all zip files in a folder into respective folders

--- code-snippets.sh
+++ code-snippets.sh
@@ -3,3 +3,6 @@ find . -name .svn -exec rm -rf {} \;
 
 # replace a string pattern
 sed s/origin/new/g filename > newfile
+
+# unzip all zip files in a folder into respective folders
+find . -name '*.zip' -exec sh -c 'unzip -d `basename {} .zip` {}' ';'

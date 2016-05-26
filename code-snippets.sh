37cdf6096f37c462214519eef3b4471ed476970d
RobertTheNerd <robert.q.wang@gmail.com>
2016-05-25 23:13:08 -0700

    Update code-snippets.sh

--- code-snippets.sh
+++ code-snippets.sh
@@ -6,3 +6,9 @@ sed s/origin/new/g filename > newfile
 
 # unzip all zip files in a folder into respective folders
 find . -name '*.zip' -exec sh -c 'unzip -d `basename {} .zip` {}' ';'
+
+# add current user's pub key to remote authorized_keys
+cat ~/.ssh/id_rsa.pub | ssh USER@HOST "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
+
+# replace a string in a file
+sed -i -- 's/foo/bar/g' *
